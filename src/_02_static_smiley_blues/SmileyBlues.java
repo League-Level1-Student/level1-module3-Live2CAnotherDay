c0e92b150e93dba6f3802d3fffbf39cf4e90c3c1
league <league@imac-24.attlocal.net>
2023-03-25 16:32:13 -0700

    Finish the netflix thingie

--- src/_02_static_smiley_blues/SmileyBlues.java
+++ src/_02_static_smiley_blues/SmileyBlues.java
@@ -12,25 +12,36 @@ public class SmileyBlues {
 		frame = new JFrame();
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
 		panel = new JPanel();
 		
+		frame.add(panel);
 		
+		
+		Smiley Yellow = new Smiley();
+		panel.add(Yellow);
+		Smiley Blue = new Smiley();
+		panel.add(Blue);
+		Smiley Green = new Smiley();
+		panel.add(Green);
+		
+		
+		
+		frame.pack();
 		// 2. Set your frame's default close operation to JFrame.EXIT_ON_CLOSE
 
 		// 3. Make a new JPanel
 
 		// 4. Add your panel to your frame
 
-		// 8. DO THIS LAST: 
-		// Set the Smiley class's color variable to Smiley.YELLOW (NOT a specific smiley object's!)
-		// Using a specific object for a static variable works, but is not usually recommended.
-		// Run it again; are your Smileys all yellow now? Also try changing them to red or green!
-
 		// 5. Make three Smiley objects and add them to your panel
 
 		// 6. Pack your frame
 
 		// 7. RUN THE PROGRAM and make sure you see three blue Smileys!
+		
+		// 8. DO THIS LAST: 
+				// Set the Smiley class's color variable to Smiley.YELLOW (NOT a specific smiley object's!)
+				// Using a specific object for a static variable works, but is not usually recommended.
+				// Run it again; are your Smileys all yellow now? Also try changing them to red or green!
 	}
 }
