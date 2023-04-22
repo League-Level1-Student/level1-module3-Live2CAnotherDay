8574dd9594b347aba27157b6519103f3d9bc02ae
Koster2411 <nk33233@pacificcoastacademy>
2023-04-22 16:30:03 -0700

    Finish the frogger

--- src/_06_frogger/Frogger.java
+++ src/_06_frogger/Frogger.java
@@ -5,7 +5,8 @@ import processing.core.PApplet;
 public class Frogger extends PApplet {
     static final int WIDTH = 800;
     static final int HEIGHT = 600;
-    
+    int x = 400;
+    int y = 300; 
     @Override
     public void settings() {
         size(WIDTH, HEIGHT);
@@ -19,11 +20,34 @@ public class Frogger extends PApplet {
     @Override
     public void draw() {
     	background(0,0,0);
-    	fill(255, 0, 0);
-    	ellipse(x, y, width, height);
+    	fill(0, 255, 0);
+    	ellipse(x, y, 25, 25);
 
     }
     static public void main(String[] args) {
         PApplet.main(Frogger.class.getName());
+    }
+    public void keyPressed(){
+        if(key == CODED){
+            if(keyCode == UP && y >= 0)
+            {
+                y = y - 5;//Frog Y position goes up
+            }
+            else if(keyCode == DOWN && y <= 800)
+            {
+                y = y + 5;
+            	//Frog Y position goes down 
+            }
+            else if(keyCode == RIGHT && x <=800 )
+            {
+            	x = x + 5;
+                //Frog X position goes right
+            }
+            else if(keyCode == LEFT && x >=0)
+            {
+            	x = x - 5;
+                //Frog X position goes left
+            }
+        }
     }
 }
